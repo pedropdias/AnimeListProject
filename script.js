140ea530020859eff6843da9fea756cce0681778
Pedro Dias <130077535+pedropdias@users.noreply.github.com>
2023-10-06 00:28:03 -0300

    layout novo e funcionalidades

--- script.js
+++ script.js
@@ -3,6 +3,31 @@ const cancelButton = document.querySelector(".cancelButton");
 const searchBox = document.querySelector(".searchBox");
 const searchInput = document.querySelector("#searchInput");
 const searchData = document.querySelector(".searchData");
+const banner = document.querySelector(".banner");
+
+//consts searchList
+const containerList = document.querySelector(".containerList");
+const searchList = document.querySelector("#searchList");
+const animeTitle = document.querySelector(".animeTitle");
+const animeJapTitle = document.querySelector(".animeJapTitle");
+const episodes = document.querySelector(".episodes");
+const genres = document.querySelector(".genres");
+const animeImg = document.querySelector(".animeImg");
+const studios = document.querySelector(".studios");
+const aired = document.querySelector(".aired");
+const synopsis = document.querySelector(".synopsis");
+const animeUrl = document.querySelector(".animeUrl");
+const searchListIconInline = document.querySelector("#searchListIconInline");
+const searchListIconGrid = document.querySelector("#searchListIconGrid");
+
+// page scroll
+
+window.addEventListener("scroll", function(){
+  let header = document.querySelector(".header")
+  header.classList.toggle('roll', window.scrollY > 0)
+})
+
+// 
 
 searchButton.onmouseover = () => {
   searchBox.classList.add("active");
@@ -21,5 +46,86 @@ cancelButton.onclick = () => {
 }
 
 searchButton.onclick = () => {
-  searchData.classList.add("active");
+  // searchData.classList.add("active");
+  if(searchInput.value.length != 0){
+    searchAnime();
+    renderSearch();
+    banner.style.display = "none";
+  }else{
+    console.log(searchInput.value.length);
+  }
+}
+
+searchListIconInline.onclick = () => {
+  searchListIconInline.style.backgroundColor = "white";
+  searchListIconInline.style.color = "rgb(26, 26, 26)";
+  searchListIconGrid.style.backgroundColor = "";
+  searchListIconGrid.style.color = "white";
+  searchList.classList.remove("searchListGrid");
+}
+
+searchListIconGrid.onclick = () => {
+  searchListIconGrid.style.backgroundColor = "white";
+  searchListIconGrid.style.color = "rgb(26, 26, 26)";
+  searchListIconInline.style.backgroundColor = "transparent";
+  searchListIconInline.style.color = "white";
+  searchList.classList.add("searchListGrid");
+}
+
+searchInput.addEventListener('keyup', function(e){
+  var key = e.which || e.keyCode;
+  if (key == 13) {
+    if(searchInput.value.length != 0){
+      searchAnime();
+      renderSearch();
+      banner.style.display = "none";
+    }else{
+      console.log(searchInput.value.length);
+    }
+  }
+});
+
+const searchAnime = async () => {
+  const query = searchInput.value.replaceAll(" ", "+");
+  const response = await fetch(`https://api.jikan.moe/v4/anime?q=${query}`)
+                      .then(response => response.json())
+                      .then(json => json)
+                      .catch(erro => console.log(erro));
+  console.log(response);
+  return response
+}
+
+
+const renderSearch = async () => {
+  // await searchAnime();
+
+  const searchResult = await searchAnime();
+  console.log(searchResult);
+
+  if(searchResult.data.length > 0) {
+    searchListIconInline.style.display = "inline-block";
+    searchListIconGrid.style.display = "inline-block";
+    document.getElementById("searchResultText").innerHTML = `<h1>Search Results for "${searchInput.value}"</h1>`;
+    document.getElementById("searchList").innerHTML = searchResult.data.map((item) => {
+      return(
+        `<div class="animeItem">
+          <img width="120px" src="${item.images.jpg.image_url}" class="animeImg" alt="">
+          <div class="animeTitleWrapper">
+            <h1 class="animeTitle">
+            <a href="index.html">
+              ${item.title}
+            </a>  
+            </h1>
+            <h2 class="animeEnglishTitle">
+            <a href="index.html" target="_blank">
+              ${item.title_english ? item.title_english : ""}
+            </a>
+            </h2>
+          </div>
+        </div>`
+      )
+    }).join('');
+  } else {
+    document.getElementById("searchList").innerHTML = '<h1 class="animeTitle">No anime today :/</h1>';
+  }
 }
